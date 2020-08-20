1578733883003e59c7e638d0a1a541eed33b8de2
Aaron J. Smith <onsmith@cs.unc.edu>
2020-08-20 12:43:23 -0400

    Initial commit

---
+++ src/main/java/com/comp301/lec04/App.java
@@ -0,0 +1,11 @@
+package com.comp301.lec04;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/main/java/com/comp301/lec04/Point.java
@@ -0,0 +1,30 @@
+package com.comp301.lec04;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  private static final double EPSILON = 0.001;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getQuadrant() {
+    // TODO
+  }
+
+  public int equals(Point other) {
+    // TODO
+  }
+
+  public double distanceTo(Point other) {
+    // TODO
+  }
+
+  public static double distance(Point a, Point b) {
+    // TODO
+  }
+}
